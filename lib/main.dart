8f8d6e80bcdc12b1a51e32c8c3db6b15b5b8a338
saul201287 <84940926+saul201287@users.noreply.github.com>
2025-06-06 18:08:00 -0600

    limpio

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:product_list_app/presentation/screens/home_screen.dart';
 import 'presentation/screens/login_screen.dart';
-import 'package:no_screenshot/no_screenshot.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,38 +18,12 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final _noScreenshot = NoScreenshot.instance;
   User? _currentUser;
 
   @override
   void initState() {
     super.initState();
     _currentUser = FirebaseAuth.instance.currentUser;
-    _disableScreenshots();
-  }
-
-  @override
-  void dispose() {
-    _enableScreenshots();
-    super.dispose();
-  }
-
-  Future<void> _disableScreenshots() async {
-    try {
-      await _noScreenshot.screenshotOff();
-      print('Capturas de pantalla deshabilitadas');
-    } catch (e) {
-      print('Error al deshabilitar capturas de pantalla: $e');
-    }
-  }
-
-  Future<void> _enableScreenshots() async {
-    try {
-      await _noScreenshot.screenshotOn();
-      print('Capturas de pantalla habilitadas');
-    } catch (e) {
-      print('Error al habilitar capturas de pantalla: $e');
-    }
   }
 
   @override
@@ -58,10 +31,9 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       title: 'Product List App',
       debugShowCheckedModeBanner: false,
-      home:
-          _currentUser == null
-              ? LoginScreen()
-              : HomeScreen(firebaseUser: _currentUser, userData: null),
+      home: _currentUser == null
+          ? LoginScreen()
+          : HomeScreen(firebaseUser: _currentUser, userData: null),
     );
   }
 }
